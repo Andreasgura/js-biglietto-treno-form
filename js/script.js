9585ee23a0cd36d030d2fbf83f01f55f1803e544
Andreasgura <andre-56@hotmail.it>
2024-03-07 16:02:50 +0100

    variables

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*
+Scrivere un programma che chieda all’utente:
+- Il numero di chilometri da percorrere
+- Età del passeggero
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+*/
+
+let elUserName = document.getElementById("username");
+let elDistance = document.getElementById("distance");
+let elAge = document.getElementById("age");
+console.log(elUserName, elDistance, elAge)
+let elPrintTicket = document.querySelector(".btn")
+
+let price = elDistance*0.21
+let priceUnder18 = price*(1-0.2)
+let priceOver65 = price*(1-0.4)
+
+console.log(price)
+
